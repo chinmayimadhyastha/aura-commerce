0b80c7d5b1a55976ac88d30d20237eec45a2ba82
chinmayimadhyastha <schimadhyastha@gmail.com>
2026-03-14 11:44:43 +0530

    fix socket cors for production

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://auracommerce-one.vercel.app"
+    ],
     methods: ["GET", "POST"],
   },
 });
